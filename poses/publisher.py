d3b22f569d8fc8073feea19f6d4ccc3bfaad29ac
sina-behnam <sina.behnam.ac@gmail.com>
2026-04-24 12:08:32 +0200

    init

---
+++ poses/publisher.py
@@ -0,0 +1,152 @@
+import numpy as np
+import os
+from tqdm import tqdm
+
+from graspnetAPI.utils.xmlhandler import xmlReader
+from graspnetAPI.utils.utils import get_obj_pose_list, generate_views, get_model_grasps, transform_points
+from graspnetAPI.utils.rotation import batch_viewpoint_params_to_matrix
+from graspnetAPI import GraspGroup
+import open3d as o3d
+
+TOTAL_SCENE_NUM = 190
+GRASP_HEIGHT = 0.02
+FRICTION_COEF_THRESH = 0.4
+
+CAMERA_POSES = '/home/sbehnam/Project/data/scenes/scene_0000/kinect/camera_poses.npy'
+CAMERA_2_TABLE = '/home/sbehnam/Project/data/scenes/scene_0000/kinect/cam0_wrt_table.npy'
+
+COLLISION_LABEL_PATH = '/home/sbehnam/Project/data/scenes/scene_0000/collision_labels.npz'
+GRASP_LABEL_PATH     = '/home/sbehnam/Project/data/scenes/scene_0000/grasp_labels/'
+FIRST_ANNOTATION_XML = '/home/sbehnam/Project/data/scenes/scene_0000/kinect/annotations/0000.xml'
+
+def loadCollisionLabel(collision_label_path):
+    print("Loading collision labels ...")
+    collision_label = np.load(collision_label_path)
+    collisionLabel = []
+    for j in range(len(collision_label)):
+        collisionLabel.append(collision_label['arr_{}'.format(j)])
+    return collisionLabel
+
+def load_grasp_label(grasp_label_path, object_ids):
+    graspLabels = {}    
+    for i in tqdm(object_ids, desc="Loading grasp labels ..."):
+        file = np.load(
+            os.path.join(
+                grasp_label_path,
+                '{}_labels.npz'.format(str(i).zfill(3))
+            )
+        )
+        graspLabels[i] = (
+            file['points'].astype(np.float32),
+            file['offsets'].astype(np.float32),
+            file['scores'].astype(np.float32)
+        )
+    return graspLabels
+
+def cam_pose(camera_poses_path, camera_2_table_path):
+    camera_poses = np.load(camera_poses_path)
+    align_mat    = np.load(camera_2_table_path)
+    return align_mat @ camera_poses[0]  # Always use the first camera pose !!!
+
+def pose2vectors(annotation_xml_path):
+    xml_reader  = xmlReader(annotation_xml_path)
+    return xml_reader.getposevectorlist()
+
+def publish_grasp_labels(wanted_object_ids : list):
+    camera_pose = np.load(CAMERA_POSES)[0]
+    pose_vectors = pose2vectors(FIRST_ANNOTATION_XML)
+
+    obj_list, pose_list = get_obj_pose_list(camera_pose, pose_vectors)
+
+    grasp_labels = load_grasp_label(GRASP_LABEL_PATH, obj_list)
+    collision_dump = loadCollisionLabel(COLLISION_LABEL_PATH)
+
+    num_views, num_angles, num_depths = 300, 12, 4
+    template_views = generate_views(num_views)
+    template_views = template_views[np.newaxis, :, np.newaxis, np.newaxis, :]
+    template_views = np.tile(template_views, [1, 1, num_angles, num_depths, 1])
+
+    grasp_group = GraspGroup()
+    for i, (obj_idx, trans) in enumerate(zip(obj_list, pose_list)):
+        if obj_idx not in wanted_object_ids:
+            continue
+        sampled_points, offsets, fric_coefs = grasp_labels[obj_idx]
+        collision = collision_dump[i]
+        point_inds = np.arange(sampled_points.shape[0])
+
+        num_points = len(point_inds)
+        target_points = sampled_points[:, np.newaxis, np.newaxis, np.newaxis, :]
+        target_points = np.tile(target_points, [1, num_views, num_angles, num_depths, 1])
+        views = np.tile(template_views, [num_points, 1, 1, 1, 1])
+        angles = offsets[:, :, :, :, 0]
+        depths = offsets[:, :, :, :, 1]
+        widths = offsets[:, :, :, :, 2]
+
+        mask1 = ((fric_coefs <= FRICTION_COEF_THRESH) & (fric_coefs > 0) & ~collision)
+        target_points = target_points[mask1]
+        target_points = transform_points(target_points, trans)
+        target_points = transform_points(target_points, np.linalg.inv(camera_pose))
+        views = views[mask1]
+        angles = angles[mask1]
+        depths = depths[mask1]
+        widths = widths[mask1]
+        fric_coefs = fric_coefs[mask1]
+
+        Rs = batch_viewpoint_params_to_matrix(-views, angles)
+        Rs = np.matmul(trans[np.newaxis, :3, :3], Rs)
+        Rs = np.matmul(np.linalg.inv(camera_pose)[np.newaxis,:3,:3], Rs)
+
+        num_grasp = widths.shape[0]
+        scores = (1.1 - fric_coefs).reshape(-1,1)
+        widths = widths.reshape(-1,1)
+        heights = GRASP_HEIGHT * np.ones((num_grasp,1))
+        depths = depths.reshape(-1,1)
+        rotations = Rs.reshape((-1,9))
+        object_ids = obj_idx * np.ones((num_grasp,1), dtype=np.int32)
+
+        obj_grasp_array = np.hstack([scores, widths, heights, depths, rotations, target_points, object_ids]).astype(np.float32)
+
+        grasp_group.grasp_group_array = np.concatenate((grasp_group.grasp_group_array, obj_grasp_array))
+
+    return grasp_group
+
+def establish_geometry(grasp_group, cloud, num_grasp=4):
+    geometries = []
+    geometries.append(cloud)
+    interested_gg = grasp_group[grasp_group.grasp_group_array[:, -1] == 5]
+
+    sampled_grasp = interested_gg.random_sample(numGrasp=num_grasp)
+
+    geometries += sampled_grasp.to_open3d_geometry_list()
+
+def render_geometries_to_notebook(geometries, width=1280, height=720, shift_x=-0.2):
+    renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
+    renderer.scene.set_background([1, 1, 1, 1])
+    
+    mat = o3d.visualization.rendering.MaterialRecord()
+    mat.shader = "defaultUnlit"
+    
+    for i, g in enumerate(geometries):
+        renderer.scene.add_geometry(f"geom_{i}", g, mat)
+    
+    bounds = o3d.geometry.AxisAlignedBoundingBox()
+    for g in geometries:
+        bounds += g.get_axis_aligned_bounding_box()
+    
+    center = bounds.get_center()
+    extent = bounds.get_extent()
+    look_at = center + np.array([extent[0] * shift_x, 0, 0])
+    eye = look_at + np.array([0, 0, -np.linalg.norm(extent) * 0.35])
+    up = np.array([0, 1, 0])
+    
+    renderer.setup_camera(60.0, look_at.astype(np.float32),
+                          eye.astype(np.float32), up.astype(np.float32))
+    
+    img = renderer.render_to_image()
+    o3d.io.write_image("scene.png", img)
+    
+    from IPython.display import Image
+    return Image("scene.png")
+
+
+    
---
+++ scenes/grasp2scene.py
@@ -0,0 +1,230 @@
+import os
+import numpy as np
+from scipy.spatial.transform import Rotation
+from graspnetAPI.utils.xmlhandler import xmlReader
+from graspnetAPI.utils.utils import parse_posevector
+
+import argparse
+
+GRASPNET_SCENE_ROOT = '/home/sbehnam/Project/data/scenes/scene_0000'
+MODEL_DIR   = '/home/sbehnam/models'
+HAND_ASSETS = '/home/sbehnam/Project/grasp2sim/franka_emika_panda/assets'
+CAMERA      = 'kinect'
+OUTPUT_XML  = 'scene_0000.xml'
+
+class Scene:
+    def __init__(self, scene_dir, model_dir, hand_assets, camera='kinect'):
+        self.camera      = camera
+        self.scene_dir   = os.path.join(scene_dir, camera)
+        self.model_dir   = model_dir
+        self.hand_assets = hand_assets
+        self.camera_poses = np.load(os.path.join(self.scene_dir, 'camera_poses.npy'))
+        self.align_mat    = np.load(os.path.join(self.scene_dir, 'cam0_wrt_table.npy'))
+        xml_reader        = xmlReader(os.path.join(self.scene_dir, 'annotations', '0000.xml'))
+        self.posevectors  = xml_reader.getposevectorlist()
+
+    @property
+    def camera_pose(self):
+        return self.align_mat @ self.camera_poses[0]
+
+    @staticmethod
+    def mat_to_quat_wxyz(T):
+        q = Rotation.from_matrix(T[:3, :3]).as_quat()
+        return q[[3, 0, 1, 2]]
+
+    def get_obj_indexes(self):
+        return [parse_posevector(pv)[0] for pv in self.posevectors]
+
+    def _build_obj_lines(self, obj_indexes=None):
+        if isinstance(obj_indexes, int):
+            obj_indexes = [obj_indexes]
+        asset_lines, body_lines = [], []
+        for pv in self.posevectors:
+            obj_idx, obj_pose_cam = parse_posevector(pv)
+            if obj_indexes is not None and obj_idx not in obj_indexes:
+                continue
+            T       = self.camera_pose @ obj_pose_cam
+            obj_str = f'{obj_idx:03d}'
+            t       = T[:3, 3]
+            q       = self.mat_to_quat_wxyz(T)
+    
+            asset_lines.append(
+                f'    <mesh name="mesh_obj_{obj_str}" file="{self.model_dir}/{obj_str}/nontextured.stl"/>'
+            )
+            body_lines.append(f'''
+                <body name="obj_{obj_str}" pos="{t[0]:.4f} {t[1]:.4f} {t[2]:.4f}"
+                      quat="{q[0]:.4f} {q[1]:.4f} {q[2]:.4f} {q[3]:.4f}">
+                  <joint type="free" damping="0.2"/>
+                  <geom type="mesh" mesh="mesh_obj_{obj_str}"
+                        contype="1" conaffinity="1"
+                        friction="1.5 0.05 0.001"
+                        solimp="0.99 0.999 0.001" solref="0.001 1"
+                        condim="4"/>
+                </body>''')
+        return asset_lines, body_lines
+
+    def shape_xml(self, obj_indexes=None):
+        asset_lines, body_lines = self._build_obj_lines(obj_indexes)
+
+        xml = f"""<mujoco model="graspnet_scene_0000">
+        <compiler angle="radian" meshdir="/" autolimits="true"/>
+
+        <option integrator="implicitfast" impratio="10" cone="elliptic" timestep="0.002" noslip_iterations="3"/>
+
+        <visual>
+          <global offwidth="1280" offheight="720"/>
+        </visual>
+
+        <default>
+          <default class="panda">
+            <material specular="0.5" shininess="0.25"/>
+            <joint armature="0.1" damping="1" axis="0 0 1" range="-2.8973 2.8973"/>
+            <general dyntype="none" biastype="affine" ctrlrange="-2.8973 2.8973" forcerange="-87 87"/>
+            <default class="finger">
+              <joint axis="0 1 0" type="slide" range="0 0.04"/>
+            </default>
+            <default class="visual">
+              <geom type="mesh" contype="0" conaffinity="0" group="2"/>
+            </default>
+            <default class="collision">
+              <geom type="mesh" group="3" friction="2 0.05 0.01"/>
+              <default class="fingertip_pad_collision_1">
+                <geom type="box" size="0.0085 0.004 0.0085" pos="0 0.0055 0.0445" friction="2 0.05 0.01"/>
+              </default>
+              <default class="fingertip_pad_collision_2">
+                <geom type="box" size="0.003 0.002 0.003" pos="0.0055 0.002 0.05" friction="2 0.05 0.01"/>
+              </default>
+              <default class="fingertip_pad_collision_3">
+                <geom type="box" size="0.003 0.002 0.003" pos="-0.0055 0.002 0.05" friction="2 0.05 0.01"/>
+              </default>
+              <default class="fingertip_pad_collision_4">
+                <geom type="box" size="0.003 0.002 0.0035" pos="0.0055 0.002 0.0395" friction="2 0.05 0.01"/>
+              </default>
+              <default class="fingertip_pad_collision_5">
+                <geom type="box" size="0.003 0.002 0.0035" pos="-0.0055 0.002 0.0395" friction="2 0.05 0.01"/>
+              </default>
+            </default>
+          </default>
+        </default>
+
+        <asset>
+            <material class="panda" name="white"     rgba="1 1 1 1"/>
+            <material class="panda" name="off_white" rgba="0.901961 0.921569 0.929412 1"/>
+            <material class="panda" name="black"     rgba="0.25 0.25 0.25 1"/>
+
+            <mesh name="hand_c"   file="{self.hand_assets}/hand.stl"/>
+            <mesh name="hand_0"   file="{self.hand_assets}/hand_0.obj"/>
+            <mesh name="hand_1"   file="{self.hand_assets}/hand_1.obj"/>
+            <mesh name="hand_2"   file="{self.hand_assets}/hand_2.obj"/>
+            <mesh name="hand_3"   file="{self.hand_assets}/hand_3.obj"/>
+            <mesh name="hand_4"   file="{self.hand_assets}/hand_4.obj"/>
+            <mesh name="finger_0" file="{self.hand_assets}/finger_0.obj"/>
+            <mesh name="finger_1" file="{self.hand_assets}/finger_1.obj"/>
+
+            {chr(10).join(asset_lines)}
+        </asset>
+
+        <worldbody>
+            <light directional="true" pos="0 0 2" dir="0 0 -1" diffuse="1 1 1"/>
+
+            <!-- Table -->
+            <body name="table" pos="0 0 0">
+              <geom type="box" size="0.5 0.5 0.01" rgba="0.8 0.7 0.6 1"
+                    contype="1" conaffinity="1"/>
+            </body>
+
+            <!-- Hand: kinematic base. Pose set from Python via model.body_pos/body_quat.
+                 No freejoint, no mocap, no weld. Fingers remain dynamic. -->
+            <body name="hand" childclass="panda" pos="0 0 0.5" quat="1 0 0 0">
+                <inertial mass="0.73" pos="-0.01 0 0.03" diaginertia="0.001 0.0025 0.0017"/>
+                <geom mesh="hand_0" material="off_white" class="visual"/>
+                <geom mesh="hand_1" material="black"     class="visual"/>
+                <geom mesh="hand_2" material="black"     class="visual"/>
+                <geom mesh="hand_3" material="white"     class="visual"/>
+                <geom mesh="hand_4" material="off_white" class="visual"/>
+                <geom mesh="hand_c" class="collision"/>
+                <body name="left_finger" pos="0 0 0.0584">
+                  <inertial mass="0.015" pos="0 0 0" diaginertia="2.375e-6 2.375e-6 7.5e-7"/>
+                  <joint name="finger_joint1" class="finger"/>
+                  <geom mesh="finger_0" material="off_white" class="visual"/>
+                  <geom mesh="finger_1" material="black"     class="visual"/>
+                  <geom mesh="finger_0" class="collision"/>
+                  <geom class="fingertip_pad_collision_1"/>
+                  <geom class="fingertip_pad_collision_2"/>
+                  <geom class="fingertip_pad_collision_3"/>
+                  <geom class="fingertip_pad_collision_4"/>
+                  <geom class="fingertip_pad_collision_5"/>
+                </body>
+                <body name="right_finger" pos="0 0 0.0584" quat="0 0 0 1">
+                  <inertial mass="0.015" pos="0 0 0" diaginertia="2.375e-6 2.375e-6 7.5e-7"/>
+                  <joint name="finger_joint2" class="finger"/>
+                  <geom mesh="finger_0" material="off_white" class="visual"/>
+                  <geom mesh="finger_1" material="black"     class="visual"/>
+                  <geom mesh="finger_0" class="collision"/>
+                  <geom class="fingertip_pad_collision_1"/>
+                  <geom class="fingertip_pad_collision_2"/>
+                  <geom class="fingertip_pad_collision_3"/>
+                  <geom class="fingertip_pad_collision_4"/>
+                  <geom class="fingertip_pad_collision_5"/>
+                </body>
+            </body>
+
+        <!-- Scene objects -->
+        {''.join(body_lines)}
+        </worldbody>
+
+          <contact>
+            <exclude body1="hand" body2="left_finger"/>
+            <exclude body1="hand" body2="right_finger"/>
+          </contact>
+
+          <equality>
+            <!-- Keep fingers symmetric. NO weld: hand base is kinematic. -->
+            <joint joint1="finger_joint1" joint2="finger_joint2"
+                   solimp="0.95 0.99 0.001" solref="0.005 1"/>
+          </equality>
+
+          <tendon>
+            <fixed name="split">
+              <joint joint="finger_joint1" coef="0.5"/>
+              <joint joint="finger_joint2" coef="0.5"/>
+            </fixed>
+          </tendon>
+
+          <actuator>
+            <general class="panda" name="actuator8" tendon="split" forcerange="-100 100"
+                      ctrlrange="0 255" gainprm="0.01568627451 0 0" biasprm="0 -100 -10"/>
+          </actuator>
+
+        </mujoco>
+        """
+        return xml
+
+    def save_xml(self, output_path, obj_indexes=None):
+        with open(output_path, 'w') as f:
+            f.write(self.shape_xml(obj_indexes))
+        print(f'Saved → {output_path}')
+
+
+
+def main():
+
+    argparser = argparse.ArgumentParser(description='Generate MuJoCo XML for a GraspNet scene.')
+    argparser.add_argument('--scene-dir', type=str, default=GRASPNET_SCENE_ROOT, help='Path to the GraspNet scene directory (e.g., /path/to/scene_0000).')
+    argparser.add_argument('--model-dir', type=str, default=MODEL_DIR, help='Path to the directory containing object models (e.g., /path/to/models).')
+    argparser.add_argument('--hand-assets', type=str, default=HAND_ASSETS, help='Path to the directory containing hand asset files (e.g., /path/to/hand_assets).')
+    argparser.add_argument('--camera', type=str, default=CAMERA, help='Camera name to use (e.g., kinect).')
+    argparser.add_argument('--output-xml', type=str, default=OUTPUT_XML, help='Path to save the generated MuJoCo XML file (e.g., scene_0000.xml).')
+    argparser.add_argument('--obj-indexes', type=int, nargs='*', default=None, help='List of object indexes to include in the scene (e.g., --obj-indexes 0 1 2). If not specified, all objects will be included.')
+    args = argparser.parse_args()
+
+    scene = Scene(
+        scene_dir=args.scene_dir,
+        model_dir=args.model_dir,
+        hand_assets=args.hand_assets,
+        camera=args.camera,
+    )
+    scene.save_xml(args.output_xml, obj_indexes=args.obj_indexes)
+    
+if __name__ == '__main__':
+    main()
